b9a4f0d3e56239e8539446d5189fff69e20ad199
sina naghipour <style.npr@gmail.com>
2023-05-03 10:23:03 -0700

    Basic Authorization Server Has Been Completed

--- app/api/user/routes.py
+++ app/api/user/routes.py
@@ -8,6 +8,7 @@ import time
 
 users = Blueprint('users', __name__, url_prefix='/users')
 
+
 @users.route('/auth', methods=['GET'])
 def authPage():
     obj = User()
@@ -20,15 +21,23 @@ def auth():
     user = json.dumps(request.json)
     user = json.loads(user)
     
-    user['loginTime'] = str(time.asctime(time.localtime(time.time())))
-    del user['password']
+    obj = User()
+    result = obj.getUserByUsername(user['username'])
     
-    token = jwt.encode(user, 'SECRET_KEY', algorithm='HS384')
-
-    session[str(uuid.uuid4())] = token
-
-    return token
+    if result == 'User With That username Not Found':
+                
+        user['loginTime'] = str(time.asctime(time.localtime(time.time())))
+        del user['password']
     
+        token = jwt.encode(user, 'SECRET_KEY', algorithm='HS384')
+
+        session[str(uuid.uuid4())] = token
+
+        return token
+
+    return 'Wrong Credentials'
+
+
 @users.route('/logout', methods=['POST'])
 def logout():
     token = request.headers.get('Authorization')
@@ -40,27 +49,43 @@ def logout():
     return 'No Such Token Was Ever Logged In'
 
 
-@users.route('/role/<id>', methods=['GET'])
-def getRole(id):
-    pass
-
 @users.route('/checkAuth', methods=['POST'])
 def isLoggedIn():
-    pass
+    token = request.headers.get('Authorization')
+    
+    for key, item in session.items():
+        if item == token:
+            return 'User Is Logged In'
+    return 'User Is Not Logged In'
 
-@users.route('/get/<id>', methods=['GET'])
+
+@users.route('/<id>/role', methods=['GET'])
+def getRole(id):
+    obj = User()
+    result = obj.getRoleID(id)
+    return result
+
+
+@users.route('/<id>/get', methods=['GET'])
 def get(id):
-    pass
+    obj = User()
+    result = obj.getUser(id)
+    result = json.dumps(result)
+    return json.loads(result)
 
-@users.route('/add/<id>', methods=['POST'])
+
+
+@users.route('/add', methods=['POST'])
 def add(id):
     pass
 
-@users.route('/update/<id>', methods=['PUT'])
+
+@users.route('/<id>/update', methods=['PUT'])
 def update(id):
     pass
 
-@users.route('/delete/<id>', methods=['DELETE'])
+
+@users.route('/<id>/delete', methods=['DELETE'])
 def delete(id):
     pass
 
